0bc200430e86d213932a7ed80e7defd9c7a9128c
karim49 <eng.karim49@gmail.com>
2025-05-20 16:17:02 +0300

    Delete orm directory

--- orm/orm_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Connection(models.Model):
-    type = models.CharField(max_length=50)
-    connection_string = models.TextField()
-
-class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
--- orm/orm_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ConnectionView, FileUploadView
-
-urlpatterns = [
-    path('connection', ConnectionView.as_view(), name='connection'),
-    path('upload-file', FileUploadView.as_view(), name='upload-file'),
-]
